f4c389f7f1a1c775f0bf6695f7c7ef5641047783
Shane Scott <shaneradio@gmail.com>
2018-01-08 16:42:11 -0500

    one issue solved...

--- assets/javascript/giphyjs.js
+++ assets/javascript/giphyjs.js
@@ -1,9 +1,11 @@
+var topics = ["dog", "tiger", "monkey", "ape", "armadillo"];
+
+var apiKey = "9Vbhzo58898EDFt3Wz5NL1r0dNbCo0Vd"
+
 $(function () {
 	console.log("page loaded")
 	buttonMaker(topics);
 })
-
-var topics = ["dog", "tiger", "monkey", "ape"];
 
 function buttonMaker (array) {
 	$("#animalButtons").empty();
@@ -13,3 +15,27 @@ function buttonMaker (array) {
 		$("#animalButtons").append(x);
 	}
 }
+
+function makeCall (searchTerm) {
+	
+	$.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + apiKey + "&limit=10",
+        method: "get"
+      })
+
+      // set instructions for after API response is received
+
+      .done(function(response) {
+
+        console.log(response);
+
+      });
+
+}
+
+makeCall("porg");
+
+
+
+	// var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=9Vbhzo58898EDFt3Wz5NL1r0dNbCo0Vd&limit=5"
+	// var imageUrl = response.data.image_original_url;
