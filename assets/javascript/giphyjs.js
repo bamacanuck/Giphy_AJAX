1c6670fee17e10b0cd3114f2d54d53ceccdff282
Shane Scott <shaneradio@gmail.com>
2018-01-08 16:17:51 -0500

    problems, already

--- assets/javascript/giphyjs.js
+++ assets/javascript/giphyjs.js
@@ -1,3 +1,17 @@
-$(function () {
-	console.log("page loaded")
-})
+// $(document).ready() {}
+
+	$(function () {
+		console.log("page loaded")
+		buttonMaker();
+	})
+
+	var topics = ["dog", "tiger", "monkey", "ape"];
+
+	function buttonMaker (topics) {
+		$("#animalButtons").empty();
+		for (var i = 0; i < topics.length; i++) {
+			var x = $("<button>");
+			x.text(topics[i]);
+			$("#animalButtons").append(x);
+		}
+	}
